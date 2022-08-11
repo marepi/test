23b1b056a65128eff21fb27969340a4de49475a2
seoyoon hwang <a80286211@gmail.com>
2022-08-12 01:55:03 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,17 @@
+name = "콩이"
+animal = "고양이"
+age = 4
+hobby = "낮잠"
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " + name + "예요")
+print(name + "는 "+str(age)+"살이며" + hobby+ "를 매우 좋아해요")
+print(name + "는 어른일까요?" + str(is_adult))
+
+name = "황서윤"
+study = "수학"
+level = 5
+is_level = level <= 3
+print("저는" + name + "입니다") 
+#print("저의 등급은" + level + "등급 입니다")
+print(name + "은 공부를 잘할까요?" + str(is_level))
